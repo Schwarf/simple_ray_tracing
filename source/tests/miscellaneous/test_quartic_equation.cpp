5e920c530541f8b16f9825277dd3c5f5d29a9bcd
Andreas Scharf <andreas.b.scharf@gmail.com>
2021-11-21 15:21:40 +0100

    Some more unit tests for quartic equation.

--- source/tests/miscellaneous/test_quartic_equation.cpp
+++ source/tests/miscellaneous/test_quartic_equation.cpp
@@ -15,29 +15,33 @@ protected:
 	float e1_{1080};
 	std::vector<float> solutions1_{-6, -4, 5, 3};
 
-	float a2_{0.89012};
+	float a2_{1.89012};
 	float b2_{-2.1212};
 	float c2_{-0.77878};
 	float d2_{1.9089};
-	std::vector<float> solutions2_{2.37049239, -0.94489002,  0.95744711};
+	float e2_{-0.94489002};
+	std::vector<float> solutions2_{-0.96142702, 1.0274915};
 
 	float a3_{3.121};
 	float b3_{-12.};
-	float c3_{-18.90009};
-	float d3_{0.};
-	std::vector<float> solutions3_{5.04526319, -1.20034169,  0.};
+	float c3_{0.f};
+	float d3_{0.f};
+	float e3_{-1.20034169};
+	std::vector<float> solutions3_{3.85165235, -0.44747665};
 
-	float a4_{3.121};
-	float b4_{-12.};
-	float c4_{18.90009};
-	float d4_{0.};
-	std::vector<float> solutions4_{0.0};
+	float a4_{2.9871f};
+	float b4_{14.13};
+	float c4_{-110.44};
+	float d4_{-256.12313};
+	float e4_{780.01212};
+	std::vector<float> solutions4_{-7.51662383, -3.81118118,  4.62778163, 1.96968292};
 
-	float a5_{2.};
-	float b5_{-12.};
-	float c5_{0.};
-	float d5_{12.};
-	std::vector<float> solutions5_{5.82305015,  1.1074036,  -0.93045375};
+	float a5_{-2.f};
+	float b5_{0.};
+	float c5_{16.f};
+	float d5_{-291.f};
+	float e5_{630.f};
+	std::vector<float> solutions5_{-6.26497173, 2.26605988};
 
 	float a6_{-2.};
 	float b6_{0.};
@@ -50,7 +54,7 @@ protected:
 	float c7_{6.};
 	float d7_{-2.};
 
-	float epsilon_{1e-6};
+	float epsilon_{1e-5};
 
 	bool check_for_solution(float value, std::vector<float> &solutions) const
 	{
@@ -81,4 +85,43 @@ TEST_F(SetupQuarticEquation, quartic_equation_four_integer_solutions)
 	EXPECT_TRUE(check_for_solution(solver.solutions()[1], solutions1_));
 	EXPECT_TRUE(check_for_solution(solver.solutions()[2], solutions1_));
 	EXPECT_TRUE(check_for_solution(solver.solutions()[3], solutions1_));
+}
+
+TEST_F(SetupQuarticEquation, quartic_equation_two_solutions)
+{
+	c_vector<5, float> coefficients{a2_, b2_, c2_, d2_, e2_};
+	auto solver = QuarticEquation<float>(coefficients, epsilon_);
+	EXPECT_EQ(solver.number_of_solutions(), 2);
+	EXPECT_TRUE(check_for_solution(solver.solutions()[0], solutions2_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[1], solutions2_));
+}
+
+
+TEST_F(SetupQuarticEquation, quartic_equation_linear_and_quadratic_coefficient_are_zero_two_solutions)
+{
+	c_vector<5, float> coefficients{a3_, b3_, c3_, d3_, e3_};
+	auto solver = QuarticEquation<float>(coefficients, epsilon_);
+	EXPECT_EQ(solver.number_of_solutions(), 2);
+	EXPECT_TRUE(check_for_solution(solver.solutions()[0], solutions3_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[1], solutions3_));
+}
+
+TEST_F(SetupQuarticEquation, quartic_equation_four_solutions)
+{
+	c_vector<5, float> coefficients{a4_, b4_, c4_, d4_, e4_};
+	auto solver = QuarticEquation<float>(coefficients, epsilon_);
+	EXPECT_EQ(solver.number_of_solutions(), 4);
+	EXPECT_TRUE(check_for_solution(solver.solutions()[0], solutions4_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[1], solutions4_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[2], solutions4_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[3], solutions4_));
+}
+
+TEST_F(SetupQuarticEquation, quartic_equation_cubic_coefficient_is_zero_two_solutions)
+{
+	c_vector<5, float> coefficients{a5_, b5_, c5_, d5_, e5_};
+	auto solver = QuarticEquation<float>(coefficients, epsilon_);
+	EXPECT_EQ(solver.number_of_solutions(), 2);
+	EXPECT_TRUE(check_for_solution(solver.solutions()[0], solutions5_));
+	EXPECT_TRUE(check_for_solution(solver.solutions()[1], solutions5_));
 }
